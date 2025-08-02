0a4e3fcf6e774bfee0b5d8b4c64b40a9428eb2da
pragmaxim <pragmaxim@gmail.com>
2025-08-02 12:13:13 +0200

    encoding cleanup

--- macros/src/encoding.rs
+++ macros/src/encoding.rs
@@ -1,12 +1,12 @@
 pub fn serde_encoding(binary_encoding: &str) -> &str {
     match binary_encoding {
-        "hex"    => "serde_with::hex::Hex",
-        "btc_addr"    => "crate::serde_enc::Btc",
-        "base64" => "serde_with::base64::Base64",
-        "base58" => "crate::serde_enc::Base58",
-        "bech32" => "crate::serde_enc::Bech32",
+        "hex"       => "serde_with::hex::Hex",
+        "base64"    => "serde_with::base64::Base64",
+        "btc_addr"  => "redbit::serde_enc::Btc",
+        "base58"    => "redbit::serde_enc::Base58",
+        "bech32"    => "redbit::serde_enc::Bech32",
         _ => panic!(
-            "Unknown encoding '{}'. Expected 'hex', 'base64', 'base58', 'bech32' or 'btc'.",
+            "Unknown encoding '{}'. Expected 'hex', 'base64', 'base58', 'bech32' or 'btc_addr'.",
             binary_encoding
         ),
     }
