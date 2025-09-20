9f1bcb1a2c1c5ff91ba9ae096f34acd586ba5589
pragmaxim <pragmaxim@gmail.com>
2025-09-20 14:24:01 +0200

    validate at least 500 blocks for equality

--- chains/btc/src/validation.rs
+++ chains/btc/src/validation.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
-use btc::block_provider::BtcBlockProvider;
+use btc::config::BitcoinConfig;
 use btc::model_v1::*;
+use btc::rest_client::BtcClient;
 use chain::launcher;
 use chain::settings::AppConfig;
 use std::collections::HashSet;
@@ -12,16 +13,20 @@ async fn main() -> Result<()> {
 
     assert_eq!(created, false, "We validate existing storage");
 
-    let validated_height = 908244;
-    let (btc_block, _) = BtcBlockProvider::block_from_file("huge", validated_height, 3713);
+    let config = BitcoinConfig::new("config/btc").expect("Failed to load Bitcoin configuration");
+    let client = Arc::new(BtcClient::new(&config)?);
     let block_tx = Block::begin_read_ctx(&storage)?;
-    let storage_block = Block::get(&block_tx, &Height(validated_height))?.expect("Block should exist in storage");
 
-    let storage_tx_hashes: HashSet<TxHash> = storage_block.transactions.into_iter().map(|tx| tx.hash).collect();
-    let btc_tx_hashes: HashSet<TxHash> = btc_block.txdata.into_iter().map(|tx| TxHash(*tx.compute_txid().as_ref())).collect();
+    for height in 915500..915585 {
+        let cbor = client.get_block_by_height(Height(height)).await?;
+        let btc_block: bitcoin::Block = bitcoin::consensus::encode::deserialize(&cbor.raw)?;
+        let storage_block = Block::get(&block_tx, &Height(height))?.expect("Block should exist in storage");
+        let storage_tx_hashes: HashSet<TxHash> = storage_block.transactions.into_iter().map(|tx| tx.hash).collect();
+        let btc_tx_hashes: HashSet<TxHash> = btc_block.txdata.into_iter().map(|tx| TxHash(*tx.compute_txid().as_ref())).collect();
+        assert_eq!(storage_tx_hashes, btc_tx_hashes, "Transaction hashes in storage do not match those in the original block");
+    }
 
-    assert_eq!(storage_tx_hashes, btc_tx_hashes, "Transaction hashes in storage do not match those in the original block");
-
+    info!("Validation successful");
     Ok(())
 }
 
