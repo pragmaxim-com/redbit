abf62048f53237703990af3d7dbb0cea9a96a881
pragmaxim <pragmaxim@gmail.com>
2025-09-20 14:42:50 +0200

    check for sentinel addresses

--- chains/btc/src/validation.rs
+++ chains/btc/src/validation.rs
@@ -4,7 +4,6 @@ use btc::model_v1::*;
 use btc::rest_client::BtcClient;
 use chain::launcher;
 use chain::settings::AppConfig;
-use std::collections::HashSet;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -17,25 +16,33 @@ async fn main() -> Result<()> {
     let client = Arc::new(BtcClient::new(&config)?);
     let block_tx = Block::begin_read_ctx(&storage)?;
 
-    for height in 915500..915585 {
+    for height in 914000..915585 {
         let cbor = client.get_block_by_height(Height(height)).await?;
         let btc_block: bitcoin::Block = bitcoin::consensus::encode::deserialize(&cbor.raw)?;
         let storage_block = Block::get(&block_tx, &Height(height))?.expect("Block should exist in storage");
-        let storage_tx_hashes: HashSet<TxHash> = storage_block.transactions.iter().map(|tx| tx.hash).collect();
-        let btc_tx_hashes: HashSet<TxHash> = btc_block.txdata.iter().map(|tx| TxHash(*tx.compute_txid().as_ref())).collect();
+        let storage_tx_hashes: Vec<TxHash> = storage_block.transactions.iter().map(|tx| tx.hash).collect();
+        let btc_tx_hashes: Vec<TxHash> = btc_block.txdata.iter().map(|tx| TxHash(*tx.compute_txid().as_ref())).collect();
         assert_eq!(storage_tx_hashes, btc_tx_hashes, "Transaction hashes in storage do not match those in the original block");
 
-        let storage_scripts: HashSet<ScriptHash> =
+        let storage_scripts: Vec<ScriptHash> =
             storage_block.transactions.iter()
                 .flat_map(|tx| tx.utxos.iter().map(|out|out.script_hash.clone()))
-                .collect::<HashSet<ScriptHash>>();
+                .collect();
 
-        let btc_scripts: HashSet<ScriptHash> =
+        let btc_scripts: Vec<ScriptHash> =
             btc_block.txdata.iter()
                 .flat_map(|tx| tx.output.iter().map(|out|ScriptHash(out.script_pubkey.as_bytes().to_vec())))
-                .collect::<HashSet<ScriptHash>>();
+                .collect();
 
         assert_eq!(storage_scripts, btc_scripts, "Output scripts in storage do not match those in the original block");
+
+
+        let storage_sentinel_addresses: Vec<Address> =
+            storage_block.transactions.iter()
+                .flat_map(|tx| tx.utxos.iter().filter(|out|out.address.0 == btc::block_provider::SENTINEL).map(|u|u.address.clone()).into_iter())
+                .collect();
+
+        assert!(storage_sentinel_addresses.is_empty(), "There should be no sentinel addresses in storage");
     }
 
     info!("Validation successful");
