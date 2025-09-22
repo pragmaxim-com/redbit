d2b2c7abbd60281b950d8fed6785f6660c5c6e8b
pragmaxim <pragmaxim@gmail.com>
2025-09-22 21:26:00 +0200

    testing all redb databases are dropped during shutdown

--- chain/src/combine.rs
+++ chain/src/combine.rs
@@ -1,5 +1,5 @@
-use std::future::Future;
 use futures::future::join;
+use std::future::Future;
 use tokio::signal::unix::{signal, SignalKind};
 use tokio::sync::watch;
 
@@ -25,4 +25,60 @@ where
             let _ = shutdown_tx.send_replace(true);
         }
     }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum ShutdownReason {
+    AlreadyTrue,
+    ChangedToTrue,
+    SenderDropped,
+}
+
+pub async fn await_shutdown(mut rx: watch::Receiver<bool>) -> ShutdownReason {
+    if *rx.borrow() {
+        return ShutdownReason::AlreadyTrue;
+    }
+    // mark current as seen so the next `changed()` waits for a new version
+    let _ = rx.borrow_and_update();
+
+    loop {
+        match rx.changed().await {
+            Ok(()) => {
+                // mark this change as seen and check the value
+                if *rx.borrow_and_update() {
+                    return ShutdownReason::ChangedToTrue;
+                }
+                // value changed but stayed false; keep waiting
+            }
+            Err(_sender_dropped) => {
+                return ShutdownReason::SenderDropped;
+            }
+        }
+    }
+}
+
+
+#[tokio::test]
+async fn test_shutdown_helper_variants() {
+    // AlreadyTrue
+    let (tx1, rx1) = watch::channel(false);
+    let _ = tx1.send(true);
+    let r1 = await_shutdown(rx1.clone()).await;
+    assert_eq!(r1, ShutdownReason::AlreadyTrue);
+
+    // ChangedToTrue
+    let (tx2, rx2) = watch::channel(false);
+    let t2 = tokio::spawn(async move {
+        tokio::time::sleep(std::time::Duration::from_millis(20)).await;
+        let _ = tx2.send(true);
+    });
+    let r2 = await_shutdown(rx2.clone()).await;
+    let _ = t2.await;
+    assert_eq!(r2, ShutdownReason::ChangedToTrue);
+
+    // SenderDropped
+    let (tx3, rx3) = watch::channel(false);
+    drop(tx3);
+    let r3 = await_shutdown(rx3.clone()).await;
+    assert_eq!(r3, ShutdownReason::SenderDropped);
 }
