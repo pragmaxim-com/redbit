d66e0ab0c51988842b105d3c255bc996534d9393
pragmaxim <pragmaxim@gmail.com>
2025-11-18 09:22:32 +0100

    ltc and bch readme and fixes

---
+++ chains/bch/README.md
@@ -0,0 +1,37 @@
+### Bitcoin Cache
+
+Download Bitcoin Cash Node:
+```
+wget https://github.com/bitcoin-cash-node/bitcoin-cash-node/releases/download/v28.0.1/bitcoin-cash-node-28.0.1-x86_64-linux-gnu.tar.gz
+```
+
+Configure it :
+```
+$ cat /opt/bitcoincash/bitcoin.conf
+server=1
+rest=1
+rpcbind=127.0.0.1
+rpcallowip=127.0.0.1
+rpcuser=foo
+rpcpassword=bar
+disablewallet=1
+maxconnections=32
+rpcport=7332
+port=7333
+```
+
+Export RPC credentials :
+```
+export BITCOINCASH__rpc_user = "foo"
+export BITCOINCASH__rpc_password = "bar"
+```
+
+Start the node :
+```
+/opt/bitcoincash/bin/bitcoind -datadir=/opt/bitcoincash -conf=/opt/bitcoincash/bitcoin.conf -daemon=0 -printtoconsole
+```
+
+Start syncing : 
+```
+cargo run --release
+```
--- chains/ltc/src/block_provider.rs
+++ chains/ltc/src/block_provider.rs
@@ -12,6 +12,7 @@ use redbit::info;
 use redbit::*;
 use serde_json;
 use std::{fs, sync::Arc};
+use litcoin::hashes::Hash;
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::watch;
 use tokio::task::JoinHandle;
@@ -34,10 +35,9 @@ impl LtcBlockProvider {
 
     // Helper used in benches/tests to load canned blocks from JSON (same format as BTC benches)
     #[allow(dead_code)]
-    pub fn block_from_file(size: &str, height: u32, tx_count: usize) -> (bitcoin::Block, LtcCBOR) {
-        use bitcoin::Block;
-        use bitcoin::block::Header as BlockHeader;
-        use bitcoin::CompactTarget;
+    pub fn block_from_file(size: &str, height: u32, tx_count: usize) -> (litcoin::Block, LtcCBOR) {
+        use litcoin::Block;
+        use litcoin::BlockHeader as BlockHeader;
         use std::str::FromStr;
 
         info!("Getting {} block with {} txs", size, tx_count);
@@ -54,15 +54,15 @@ impl LtcBlockProvider {
         let bits = u32::from_str_radix(bits_hex, 16).expect("invalid bits");
         let nonce = v["nonce"].as_u64().unwrap() as u32;
 
-        let prev_hash = bitcoin::BlockHash::from_str(prev_hash_s).expect("invalid prev hash");
-        let merkle_root = bitcoin::hash_types::TxMerkleNode::from_str(merkle_root_s).expect("invalid merkle root");
+        let prev_hash = litcoin::BlockHash::from_str(prev_hash_s).expect("invalid prev hash");
+        let merkle_root = litcoin::hash_types::TxMerkleNode::from_str(merkle_root_s).expect("invalid merkle root");
 
         let header = BlockHeader {
-            version: bitcoin::block::Version::from_consensus(version),
+            version,
             prev_blockhash: prev_hash,
             merkle_root,
             time,
-            bits: CompactTarget::from_consensus(bits),
+            bits,
             nonce,
         };
 
@@ -72,18 +72,18 @@ impl LtcBlockProvider {
             for txv in arr {
                 let tx_hex = txv["hex"].as_str().expect("missing tx hex");
                 let tx_bytes = hex::decode(tx_hex).expect("invalid tx hex");
-                let tx: bitcoin::Transaction = bitcoin::consensus::encode::deserialize(&tx_bytes).expect("tx deser");
+                let tx: litcoin::Transaction = litcoin::consensus::encode::deserialize(&tx_bytes).expect("tx deser");
                 txs.push(tx);
             }
         }
 
         let block = Block { header, txdata: txs };
-        let raw = bitcoin::consensus::encode::serialize(&block);
+        let raw = litcoin::consensus::encode::serialize(&block);
         (block.clone(), LtcCBOR { height: Height(height), raw })
     }
 
     pub fn process_block_pure(cbor: &LtcCBOR) -> Result<Block, ChainError> {
-        let block: bitcoin::Block = bitcoin::consensus::encode::deserialize(&cbor.raw).map_err(|e| ChainError::new(&format!("Failed to deser CBOR: {}", e)))?;
+        let block: litcoin::Block = litcoin::consensus::encode::deserialize(&cbor.raw).map_err(|e| ChainError::new(&format!("Failed to deser CBOR: {}", e)))?;
         let height = cbor.height;
         let mut block_weight = 6;
         let transactions = block
@@ -99,19 +99,19 @@ impl LtcBlockProvider {
         let header = Header {
             height: height.clone(),
             timestamp: Timestamp(block.header.time),
-            hash: BlockHash(*block.block_hash().as_ref()),
-            prev_hash: BlockHash(*block.header.prev_blockhash.as_ref()),
-            merkle_root: MerkleRoot(*block.header.merkle_root.as_ref()),
+            hash: BlockHash(block.block_hash().as_hash().into_inner()),
+            prev_hash: BlockHash(block.header.prev_blockhash.as_hash().into_inner()),
+            merkle_root: MerkleRoot(block.header.merkle_root.as_hash().into_inner()),
             weight: Weight(block_weight as u32),
         };
 
         Ok(Block { height, header, transactions })
     }
 
-    fn process_inputs(ins: &[bitcoin::TxIn]) -> Vec<InputRef> {
+    fn process_inputs(ins: &[litcoin::TxIn]) -> Vec<InputRef> {
         ins.iter()
             .map(|input| {
-                let tx_hash = TxHash(*input.previous_output.txid.as_ref());
+                let tx_hash = TxHash(input.previous_output.txid.as_hash().into_inner());
                 InputRef { tx_hash, index: input.previous_output.vout as u16 }
             })
             .collect()
@@ -122,8 +122,7 @@ impl LtcBlockProvider {
     /// Legacy version bytes differ from Bitcoin:
     /// - P2PKH: 0x30
     /// - P2SH:  0x32
-    pub fn spk_to_address_bytes_or_tag(spk: &bitcoin::Script) -> Vec<u8> {
-        use bitcoin::blockdata::script::witness_version::WitnessVersion;
+    pub fn spk_to_address_bytes_or_tag(spk: &litcoin::Script) -> Vec<u8> {
         let b = spk.as_bytes();
 
         // Legacy P2PKH
@@ -146,9 +145,8 @@ impl LtcBlockProvider {
         }
         // Segwit
         if spk.is_witness_program() {
-            let opcode = spk.first_opcode().expect("witness_program => len > 4");
-            if let Ok(wv) = WitnessVersion::try_from(opcode) {
-                let ver  = wv.to_num() as u8;
+            if let Some(wv) = spk.witness_version() {
+                let ver = wv.into_num() as u8;
                 let prog = &b[2..];
                 if prog.len() == 20 || prog.len() == 32 {
                     let mut v = Vec::with_capacity(2 + prog.len());
@@ -166,14 +164,14 @@ impl LtcBlockProvider {
         vec![TAG_NON_ADDR]
     }
 
-    fn process_outputs(outs: &[bitcoin::TxOut], tx_pointer: BlockPointer) -> (BoxWeight, Vec<Utxo>) {
+    fn process_outputs(outs: &[litcoin::TxOut], tx_pointer: BlockPointer) -> (BoxWeight, Vec<Utxo>) {
         let mut result_outs = Vec::with_capacity(outs.len());
         for (out_index, out) in outs.iter().enumerate() {
-            let script = out.script_pubkey.as_script();
+            let script = &out.script_pubkey;
             let address = Self::spk_to_address_bytes_or_tag(script);
             result_outs.push(Utxo {
                 id: TransactionPointer::from_parent(tx_pointer, out_index as u16),
-                amount: out.value.to_sat().into(),
+                amount: out.value.into(),
                 script_hash: ScriptHash(out.script_pubkey.as_bytes().to_vec()),
                 address: Address(address),
             })
@@ -181,12 +179,12 @@ impl LtcBlockProvider {
         (result_outs.len(), result_outs)
     }
 
-    fn process_tx(height: Height, tx_index: u16, tx: &bitcoin::Transaction) -> Transaction {
+    fn process_tx(height: Height, tx_index: u16, tx: &litcoin::Transaction) -> Transaction {
         let tx_pointer = BlockPointer::from_parent(height, tx_index);
         let (_, outputs) = Self::process_outputs(&tx.output, tx_pointer);
         Transaction {
             id: tx_pointer,
-            hash: TxHash(*tx.compute_txid().as_ref()),
+            hash: TxHash(tx.txid().as_hash().into_inner()),
             utxos: outputs,
             inputs: vec![],
             input_refs: Self::process_inputs(&tx.input),
@@ -225,5 +223,56 @@ impl BlockProvider<LtcCBOR, Block> for LtcBlockProvider {
         batch: bool,
     ) -> (Receiver<Vec<LtcCBOR>>, JoinHandle<()>) {
         self.block_stream.stream(remote_chain_tip_header, last_persisted_header, shutdown, batch)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use litcoin::blockdata::script::Script;
+    use litcoin::hash_types::{PubkeyHash, ScriptHash as LScriptHash, WPubkeyHash, WScriptHash};
+
+    #[test]
+    fn p2pkh_tag_matches_litecoin_version_byte() {
+        let h = PubkeyHash::from_slice(&[0x11; 20]).unwrap();
+        let spk = Script::new_p2pkh(&h);
+        let out = LtcBlockProvider::spk_to_address_bytes_or_tag(&spk);
+        assert_eq!(out[0], 0x30);
+        assert_eq!(&out[1..], &h[..]);
+    }
+
+    #[test]
+    fn p2sh_tag_matches_litecoin_version_byte() {
+        let h = LScriptHash::from_slice(&[0x22; 20]).unwrap();
+        let spk = Script::new_p2sh(&h);
+        let out = LtcBlockProvider::spk_to_address_bytes_or_tag(&spk);
+        assert_eq!(out[0], 0x32);
+        assert_eq!(&out[1..], &h[..]);
+    }
+
+    #[test]
+    fn segwit_v0_20_and_32() {
+        let wpkh = WPubkeyHash::from_slice(&[0x33; 20]).unwrap();
+        let spk20 = Script::new_v0_p2wpkh(&wpkh);
+        let out20 = LtcBlockProvider::spk_to_address_bytes_or_tag(&spk20);
+        assert_eq!(out20[0], TAG_SEGWIT);
+        assert_eq!(out20[1], 0);
+        assert_eq!(&out20[2..], &wpkh[..]);
+
+        let wsh = WScriptHash::from_slice(&[0x44; 32]).unwrap();
+        let spk32 = Script::new_v0_p2wsh(&wsh);
+        let out32 = LtcBlockProvider::spk_to_address_bytes_or_tag(&spk32);
+        assert_eq!(out32[0], TAG_SEGWIT);
+        assert_eq!(out32[1], 0);
+        assert_eq!(&out32[2..], &wsh[..]);
+    }
+
+    #[test]
+    fn op_return_and_non_addr() {
+        let op = Script::new_op_return(&[1,2,3]);
+        assert_eq!(LtcBlockProvider::spk_to_address_bytes_or_tag(&op), vec![TAG_OP_RETURN]);
+
+        let empty = Script::new();
+        assert_eq!(LtcBlockProvider::spk_to_address_bytes_or_tag(&empty), vec![TAG_NON_ADDR]);
     }
 }
